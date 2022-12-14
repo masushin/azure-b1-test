06de5782c27b8d6ebfb0dd85164219f117cec04a
Masuda Shinsuke <shinsuke.masuda@gmail.com>
2022-12-15 00:17:50 +0900

    update

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 import os
 from nicegui import ui
 
-toggle1 = ui.toggle([1, 2, 3], value=1)
-toggle2 = ui.toggle({1: 'A', 2: 'B', 3: 'C'}).bind_value(toggle1, 'value')
+@ui.page('/rosenka2')
+def pageRosenka2():
+    ui.markdown('''
+## 路線価図2
+*****
+    ''')
+
+@ui.page('/rosenka4')
+def pageRosenka4():
+    ui.markdown('''
+## 路線価図4
+*****
+    ''')
+
+ui.markdown('''
+# PDF Tools
+*****
+### 路線価図
+''')
+ui.link("２枚の路線価図をつなげる",pageRosenka2)
+ui.link("４枚の路線価図をつなげる",pageRosenka4)
 
 print(os.environ['PORT'])
 ui.run(port=int(os.environ['PORT']))
+
